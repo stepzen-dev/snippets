a835b5dcc24373391d340f9d6d0f7d49d1e41fba
Bobbie Cochrane <webbobbie@fordcochrane.com>
2022-07-07 13:14:31 -0400

    added function to programitically create teh test description from the full __dirname, and fixed enumerated type name to match the other exports (#12)

--- sequence/arguments/tests/Test.js
+++ sequence/arguments/tests/Test.js
@@ -1,19 +1,22 @@
 const {
   deployAndRun,
-  AuthType,
+  authTypes,
+  getTestDescription,
 } = require("../../../tests/gqltest.js");
 
-describe("sequence/arguments", function () {
+testDescription = getTestDescription("snippets", __dirname);
+
+describe(testDescription, function () {
   const tests = [
     { label: "customer(1)", 
       query: "{customer(id:1){name city weather{temp}}}", 
       expected: {customer: { name: "John Doe", city: "Miami", weather: { temp: 100.0, }}},
-      authType: AuthType.adminKey,
+      authType: authTypes.adminKey,
     },
     { label: "customer(2)",
       query: "{customer(id:2){name city weather{temp}}}",
       expected:  {customer: {name: "Jane Smith", city: "Santa Clara", weather: {temp: 60.4, },},},
-      authType: AuthType.adminKey,
+      authType: authTypes.adminKey,
     },
   ]
   return deployAndRun(__dirname, tests);
