a546a3bc58dbde8d30689e5fe60ff0eb109f1d8a
Bobbie Cochrane <bobbie@stepzen.com>
2022-08-18 18:45:49 -0400

    using the right single tick

--- sequence/arguments/tests/Test.js
+++ sequence/arguments/tests/Test.js
@@ -9,18 +9,17 @@ testDescription = getTestDescription("snippets", __dirname);
 describe(testDescription, function () {
   const tests = [
     { label: "customer(1)", 
-      query: `{customer(id:1){name city weather{temp}}}`, 
+      query: '{customer(id:1){name city weather{temp}}}', 
       expected: {customer: { name: "John Doe", city: "Miami", weather: { temp: 100.0, }}},
       authType: authTypes.adminKey,
     },
     { label: "customer(2)",
-      query: `{customer(id:2){name city weather{temp}}}`,
+      query: '{customer(id:2){name city weather{temp}}}',
       expected:  {customer: {name: "Jane Smith", city: "Santa Clara", weather: {temp: 60.4, },},},
       authType: authTypes.adminKey,
     },
     { label: "customer(1) metric, customer(2) metric",
-      query: `{customer1: customer(id: "1") {city name weather(units: "metric") {temp}}
-             customer2: customer(id: "2") {city name weather(units: "metric") {temp}}}`,
+      query: '{customer1: customer(id: "1") {city name weather(units: "metric") {temp}} customer2: customer(id: "2") {city name weather(units: "metric") {temp}}}',
       expected:   {customer1: {city: "Miami", name: "John Doe", weather: {temp: 37.77777777777778}},
                  customer2: {city: "Santa Clara", name: "Jane Smith", weather: {temp: 15.777777777777779}}},
       authType: authTypes.adminKey,
