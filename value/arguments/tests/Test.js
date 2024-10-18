d91af874be89fc373e01bf05789426bcefc4f1c5
jayaramv99 <jayaram.venkatanarayanan@ibm.com>
2024-10-18 10:43:29 +0530

    resolved review comments

--- value/arguments/tests/Test.js
+++ value/arguments/tests/Test.js
@@ -24,6 +24,14 @@ const {
         query: '{pi}',
         expected: {pi:3.14159},
       },
+      { label: "return sum of two input using @value with script-json",
+        query: '{sum(a: 10, b: 10)}',
+        expected: {sum: 20},
+      },
+      { label: "concat string",
+        query: '{concat(a: "Steve",b:"Jobs" )}',
+        expected: {concat: 'Steve-Jobs'},
+      },
     ]
     return deployAndRun(__dirname, tests, stepzen.admin);
   });
