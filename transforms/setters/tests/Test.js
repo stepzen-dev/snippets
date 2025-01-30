72a127854d6b75c6e0d376d50d6a035d9555cc9b
asararatnakar <asara.ratnakar@gmail.com>
2025-01-30 18:54:12 -0500

    minor edits:
    Signed-off-by: asararatnakar <asara.ratnakar@gmail.com>

--- transforms/setters/tests/Test.js
+++ transforms/setters/tests/Test.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 const path = require("node:path");
 const {
   deployAndRun,
@@ -8,15 +8,15 @@ const {
 
 testDescription = getTestDescription("snippets", __dirname);
 
-const requestsFile = path.join(path.dirname(__dirname), 'requests.graphql');
-const requests = fs.readFileSync(requestsFile, 'utf8').toString();
+const requestsFile = path.join(path.dirname(__dirname), "requests.graphql");
+const requests = fs.readFileSync(requestsFile, "utf8").toString();
 
 describe(testDescription, function () {
   const tests = [
     {
       label: "customer-by-id-1",
       query: requests,
-      operationName: 'CustomerByID',
+      operationName: "CustomerByID",
       variables: { id: 1 },
       expected: {
         customerByID: {
@@ -24,19 +24,16 @@ describe(testDescription, function () {
             city: "Raleigh",
             country: "USA",
             state: "NC",
-            street: "101 Main St",
-            postalCode: "54321"
           },
           cId: "12345",
-          cName: "John Doe"
-
-        }
-      }
+          cName: "John Doe",
+        },
+      },
     },
     {
       label: "customer-by-id-100",
       query: requests,
-      operationName: 'CustomerByID',
+      operationName: "CustomerByID",
       variables: { id: 100 },
       expected: {
         customerByID: {
@@ -44,15 +41,12 @@ describe(testDescription, function () {
             city: "Hyderabad",
             country: "India",
             state: "TS",
-            street: "J.N.T.U Colony",
-            postalCode: "654231"
           },
           cId: "21345",
-          cName: "Siddarth A"
-
-        }
-      }
-    }
-  ]
+          cName: "Siddarth A",
+        },
+      },
+    },
+  ];
   return deployAndRun(__dirname, tests, stepzen.admin);
 });
